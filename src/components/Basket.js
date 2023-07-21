3d15789a27f6e17ec90a8007ed3eb5bee28b6684
Ming Chi <wong_mingchi@yahoo.co.uk>
2023-07-22 00:20:56 +0100

    Use effect hook added to check change in state

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -1,39 +1,54 @@
 import React, { useState, useMemo, useEffect } from 'react';
 
+import React from 'react'
 
 function Basket() {
   const [basket, setBasket] = useState('');
-  const [itemPrices, setItemPrices] = useState({});
-  const [scannedItems, setScannedItems] = useState([]);
-  const [isPricingRulesValid, setPricingRulesValid] = useState(true);
-  const [isTotalCalculated, setTotalCalculated] = useState(false);
-  const [totalPrice, setTotalPrice] = useState(0);
+    const [itemPrices, setItemPrices] = useState({});
+    const [scannedItems, setScannedItems] = useState([]);
+    const [isPricingRulesValid, setPricingRulesValid] = useState(true);
+    const [isTotalCalculated, setTotalCalculated] = useState(false);
+    const [totalPrice, setTotalPrice] = useState(0);
 
-  useEffect(() => {
-    setPricingRulesValid(validatePricingRules(pricingRules));
-  }, [pricingRules]);
+    const pricingRules = useMemo(
+      () => ({
+        A: { unitPrice: 50, specialPrice: { quantity: 3, price: 130 } },
+        B: { unitPrice: 30, specialPrice: { quantity: 2, price: 45 } },
+        C: { unitPrice: 20 },
+        D: { unitPrice: 15 }, 
+      }),
+      []
+    );
+    useEffect(() => {
+      setPricingRulesValid(validatePricingRules(pricingRules));
+    }, [pricingRules]);
 
-  const pricingRules = useMemo(
-    () => ({
-      A: { unitPrice: 50, specialPrice: { quantity: 3, price: 130 } },
-      B: { unitPrice: 30, specialPrice: { quantity: 2, price: 45 } },
-      C: { unitPrice: 20 },
-      D: { unitPrice: 15 },
-    }),
-    []
-  );
+    const validatePricingRules = (rules) => {
+      for (const item of Object.keys(rules)) {
+        const itemData = rules[item];
+        if (!itemData || typeof itemData.unitPrice !== 'number') {
+          return false;
+        }
+      }
+      return true;
+    };
+
+    useEffect(() => {
+      if (!isPricingRulesValid) {
+        // Reset values when pricing rules are not valid
+        setTotalCalculated(false);
+        setTotalPrice(0);
+        setItemPrices({});
+        setScannedItems([]);
+        return;
+      }
+  
+      
+    }, []);
+    
   return (
     <div>
-      <h1>Check Out System</h1>
-      <h3>Pricing Rules:</h3>
-      <ul>
-        {Object.entries(pricingRules).map(([item, { unitPrice, specialPrice }]) => (
-          <li key={item}>
-            {item}: {unitPrice} pence {specialPrice ? `, ${specialPrice.quantity} for ${specialPrice.price} pence` : ''}
-          </li>
-        ))}
-      </ul>
-      <p>Scan items using individual letters (A, B, C, and D):</p>
+      <h1>Basket</h1>
     </div>
   )
 }
