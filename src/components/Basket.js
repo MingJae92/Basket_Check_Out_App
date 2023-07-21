f9175f7b5cde789553a65dbfb77d2db9c5b482e5
Ming Chi <wong_mingchi@yahoo.co.uk>
2023-07-21 23:38:59 +0100

    Basket component updates

---
+++ src/components/Basket.js
@@ -0,0 +1,30 @@
+import React, { useState, useMemo, useEffect } from 'react';
+
+
+function Basket() {
+  const pricingRules = useMemo(
+    () => ({
+      A: { unitPrice: 50, specialPrice: { quantity: 3, price: 130 } },
+      B: { unitPrice: 30, specialPrice: { quantity: 2, price: 45 } },
+      C: { unitPrice: 20 },
+      D: { unitPrice: 15 }, 
+    }),
+    []
+  );
+  return (
+    <div>
+      <h1>Check Out System</h1>
+      <h3>Pricing Rules:</h3>
+      <ul>
+            {Object.entries(pricingRules).map(([item, { unitPrice, specialPrice }]) => (
+              <li key={item}>
+                {item}: {unitPrice} pence {specialPrice ? `, ${specialPrice.quantity} for ${specialPrice.price} pence` : ''}
+              </li>
+            ))}
+          </ul>
+          <p>Scan items using individual letters (A, B, C, and D):</p>
+    </div>
+  )
+}
+
+export default Basket
