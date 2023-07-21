c7fb1fe1430a558821368143b221f737bf4eee08
Ming Chi <wong_mingchi@yahoo.co.uk>
2023-07-21 23:53:43 +0100

    React hooks declared

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -2,12 +2,19 @@ import React, { useState, useMemo, useEffect } from 'react';
 
 
 function Basket() {
+  const [basket, setBasket] = useState('');
+  const [itemPrices, setItemPrices] = useState({});
+  const [scannedItems, setScannedItems] = useState([]);
+  const [isPricingRulesValid, setPricingRulesValid] = useState(true);
+  const [isTotalCalculated, setTotalCalculated] = useState(false);
+  const [totalPrice, setTotalPrice] = useState(0);
+  
   const pricingRules = useMemo(
     () => ({
       A: { unitPrice: 50, specialPrice: { quantity: 3, price: 130 } },
       B: { unitPrice: 30, specialPrice: { quantity: 2, price: 45 } },
       C: { unitPrice: 20 },
-      D: { unitPrice: 15 }, 
+      D: { unitPrice: 15 },
     }),
     []
   );
@@ -16,13 +23,13 @@ function Basket() {
       <h1>Check Out System</h1>
       <h3>Pricing Rules:</h3>
       <ul>
-            {Object.entries(pricingRules).map(([item, { unitPrice, specialPrice }]) => (
-              <li key={item}>
-                {item}: {unitPrice} pence {specialPrice ? `, ${specialPrice.quantity} for ${specialPrice.price} pence` : ''}
-              </li>
-            ))}
-          </ul>
-          <p>Scan items using individual letters (A, B, C, and D):</p>
+        {Object.entries(pricingRules).map(([item, { unitPrice, specialPrice }]) => (
+          <li key={item}>
+            {item}: {unitPrice} pence {specialPrice ? `, ${specialPrice.quantity} for ${specialPrice.price} pence` : ''}
+          </li>
+        ))}
+      </ul>
+      <p>Scan items using individual letters (A, B, C, and D):</p>
     </div>
   )
 }
